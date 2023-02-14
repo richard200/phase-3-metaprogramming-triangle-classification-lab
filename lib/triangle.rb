283bb20571434194459a50a1660bbe608dedc0d1
richard200 <richmn200@gmail.com>
2023-02-14 12:18:12 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
+  attr_accessor :equitorial, :isosceles, :scalene
   # write code here
+  def initialize(opposite, hypotenuse, adjacent)
+    @opposite = opposite
+    @hypotenuse = hypotenuse
+    @adjacent = adjacent 
+  end
+  
+  def kind
+    if @hypotenuse == @opposite == @adjacent
+      :equitorial
+    elsif @hypotenuse == @opposite || @opposite == @adjacent || @hypotenuse == @adjacent
+       :isosceles
+    else
+      :scalene
+
+  end
 end
